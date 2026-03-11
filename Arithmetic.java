2bc5bc32dfebfcf86e148440c9f7f2688dc3d355
shrameeths4TH-SEM <shrameeths@gmail.com>
2026-03-11 09:45:54 +0530

    Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,20 @@
+importt java.util.Scanner;
+class sum
+  {
+    public static void main(String args[])
+    {
+      Scanner s=new Scanner(System.in);
+      System.out.println("enter two number");
+      int a=s.nextInt();
+      int b=s.nextInt();
+      int sum=a+b;
+      int mul=a*b;
+      int div=b/a;
+      int dif=a-b;
+      System.out.println("sum is"+sum);
+      System.out.println("mul is"+mul);
+      System.out.println("div is"+div);
+      System.out.println("dif is"+dif);
+    }
+  }
+    
